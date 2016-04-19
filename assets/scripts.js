a198b274018bc76fdc9d705bedca1b4471aa344c
Natan Felles <natanfelles@gmail.com>
2016-04-19 01:49:37 -0300

    Config Ace Editor

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,6 @@
+/**
+ * Google Analytics
+ */
 (function (i, s, o, g, r, a, m) {
     i.GoogleAnalyticsObject = r;
     i[r] = i[r] || function () {
@@ -11,3 +14,25 @@
 })(window, document, "script", "http://www.google-analytics.com/analytics.js", "ga");
 ga("create", "UA-75463090-1", "auto");
 ga("send", "pageview");
+
+/**
+ * Ace Editor
+ */
+// trigger extension
+ace.require("ace/ext/language_tools");
+var editor = ace.edit("editor");
+// Todo: Auto change Theme by User Config and mode by file extension
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/php");
+// enable autocompletion and snippets
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true
+});
+document.getElementById('editor').style.fontSize = '16px';
+// Infinity scroll
+editor.$blockScrolling = Infinity;
+editor.getSession().setUseWrapMode(true);
+editor.getSession().setUseSoftTabs(true);
+editor.getSession().setTabSize(4);
