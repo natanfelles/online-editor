66491c13e3646b41d2fc3578426378a6b31a5d23
Natan Felles <natanfelles@gmail.com>
2016-04-19 08:25:16 -0300

    Sync with Local

--- assets/scripts.js
+++ assets/scripts.js
@@ -36,3 +36,56 @@ editor.$blockScrolling = Infinity;
 editor.getSession().setUseWrapMode(true);
 editor.getSession().setUseSoftTabs(true);
 editor.getSession().setTabSize(4);
+
+/**
+ * Interaction
+ */
+function saveTextAsFile() {
+    var textToWrite = editor.getValue();
+    var textFileAsBlob = new Blob([textToWrite], {type: 'text/plain'});
+    var fileNameToSaveAs = document.getElementById("filename").value;
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
+
+function loadFileAsText() {
+    var fileToLoad = document.getElementById("loadfile").files[0];
+
+    var fileReader = new FileReader();
+    fileReader.onload = function (fileLoadedEvent) {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        editor.setValue(textFromFileLoaded);
+        document.getElementById("filename").value = document.getElementById("loadfile").value;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
+/**
+ * Set menu as active
+ */
+$('.dropdown-menu a').click(function () {
+    $('.dropdown-menu a').removeClass('active');
+    $(this).addClass('active');
+});
+
